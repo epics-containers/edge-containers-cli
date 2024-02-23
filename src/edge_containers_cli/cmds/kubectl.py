37adf4c3f042840b58c83bda09e71fbf56cdfafb
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-02-23 18:08:45 +0000

    Overhaul of ps command

--- src/edge_containers_cli/cmds/kubectl.py
+++ src/edge_containers_cli/cmds/kubectl.py
@@ -1,10 +1,15 @@
-json_service_info = (
+jsonpath_pod_info = (
     "-o jsonpath='"
-    r'{range .items[*]}{..labels.app}{","}{..containerStatuses[0].ready}'
-    r'{","}{..containerStatuses[0].restartCount}{","}{.status.startTime}'
+    r'{range .items[*]}{..labels.app}{","}{.status.phase}'
+    r'{","}{..containerStatuses[0].restartCount}'
     r'{"\n"}{end}'
     "'"
 )
 
-# force all the values to be strings so there are never parsing errors
-json_service_types = {"name": str, "ready": str, "restarts": str, "started": str}
+jsonpath_deploy_info = (
+    "-o jsonpath='"
+    r'{range .items[*]}{.metadata.name}{","}'
+    r"{range .spec.template.spec.containers[*]}{.image}"
+    r'{"\n"}{end}{end}'
+    "'"
+)
