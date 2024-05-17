ccc6dfd5d384013c1f28cc8a1e7761834179fe2c
Giles Knap <giles.knap@diamond.ac.uk>
2024-05-17 21:16:52 +0100

    add Commands base class

---
+++ src/edge_containers_cli/cmds/commands.py
@@ -0,0 +1,96 @@
+import webbrowser
+from pathlib import Path
+
+import polars
+import shell
+import typer
+
+import edge_containers_cli.globals as globals
+from edge_containers_cli.cmds.monitor import MonitorApp
+from edge_containers_cli.logging import log
+
+
+class Commands:
+    """
+    A base class for K8SCommands and LocalCommands
+
+    Implements the common functionality but defers specialist functions to
+    the subclasss
+
+    Allows the CLI or the TUI to call functions without worrying about local
+    vs Kubernetes containers
+    """
+
+    def __init__(self, ctx: globals.Context):
+        self.namespace = ctx.namespace
+        self.beamline_repo = ctx.beamline_repo
+
+    def attach(self, service_name):
+        raise NotImplementedError
+
+    def delete(self, service_name):
+        raise NotImplementedError
+
+    def template(self, svc_instance: Path, args: str):
+        raise NotImplementedError
+
+    def deploy_local(self, service_name):
+        raise NotImplementedError
+
+    def deploy(self, service_name: str, version: str, args: str):
+        raise NotImplementedError
+
+    def exec(self, service_name: str):
+        raise NotImplementedError
+
+    def logs(self, service_name: str, prev: bool, follow: bool):
+        raise NotImplementedError
+
+    def restart(self, service_name: str):
+        raise NotImplementedError
+
+    def start(self, service_name: str):
+        raise NotImplementedError
+
+    def stop(self, service_name: str):
+        raise NotImplementedError
+
+    def get_services(self, all: bool) -> list:
+        raise NotImplementedError
+
+    def _check_namespace(ns):
+        raise NotImplementedError
+
+    def ps(self, all: bool, wide: bool):
+        select_data = self.get_services(all)
+        services_df = polars.DataFrame(select_data)
+        if not wide:
+            services_df.drop_in_place("image")
+        print(services_df)
+
+    def monitor(self, all: bool):
+        app = MonitorApp(self, all)
+        app.run()
+
+    def environment(self, verbose: bool):
+        """
+        declare the environment settings for ec
+        """
+        ns = self.namespace
+
+        if ns == globals.LOCAL_NAMESPACE:
+            typer.echo("ioc commands deploy to the local docker/podman instance")
+        else:
+            self._check_namespace(ns)
+            typer.echo(f"ioc commands deploy to the {ns} namespace the K8S cluster")
+
+        typer.echo("\nEC environment variables:")
+        shell.run_command("env | grep '^EC_'", interactive=False, show=True)
+
+    def log_history(self):
+        if not globals.EC_LOG_URL:
+            log.error("EC_LOG_URL environment not set")
+            raise typer.Exit(1)
+
+        url = globals.EC_LOG_URL.format(service_name=self.service_name)
+        webbrowser.open(url)
