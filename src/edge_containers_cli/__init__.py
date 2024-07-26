6b1cd9f9c9656a26a7f79ef5c10db28ae250788a
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2024-07-26 17:38:49 +0100

    Move initialisation code to __init__.py

--- src/edge_containers_cli/__init__.py
+++ src/edge_containers_cli/__init__.py
@@ -1,3 +1,12 @@
 from ._version import __version__
+import polars
 
 __all__ = ["__version__"]
+
+# Set formatting of polars tables
+polars.Config.set_tbl_hide_column_data_types(True)
+polars.Config.set_tbl_hide_dataframe_shape(True)
+polars.Config.set_tbl_rows(-1)
+polars.Config.set_tbl_cols(-1)
+polars.Config.set_fmt_str_lengths(82)
+polars.Config.set_tbl_formatting("ASCII_MARKDOWN")
--- src/edge_containers_cli/globals.py
+++ src/edge_containers_cli/globals.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-import polars
-
 # common stings used in paths
 # location for config
 CONFIG_ROOT = Path(os.path.expanduser("~/.config/edge-containers-cli/"))
@@ -19,10 +17,3 @@ SERVICES_DIR = "services"
 # Shared values
 SHARED_VALUES = "beamline_values.yaml"
 
-# Set formatting of polars tables
-polars.Config.set_tbl_hide_column_data_types(True)
-polars.Config.set_tbl_hide_dataframe_shape(True)
-polars.Config.set_tbl_rows(-1)
-polars.Config.set_tbl_cols(-1)
-polars.Config.set_fmt_str_lengths(82)
-polars.Config.set_tbl_formatting("ASCII_MARKDOWN")
